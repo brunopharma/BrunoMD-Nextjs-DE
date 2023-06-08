f9e210d882ad861abb01d543ebaf8bf2793f18f6
NotoriousJat <nishantjat.dx@gmail.com>
2023-06-08 12:53:20 +0530

    witia removed t-1

--- src/utilities/WistiaHero/index.js
+++ src/utilities/WistiaHero/index.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles.module.css'
 import Link from 'next/link'
 import Image from 'next/image';
@@ -12,7 +12,8 @@ export const WistiaHero = ({ id, content }) => {
     return (
         <section className={styles.cover}>
             <div className={styles.holder}>
-                <iframe src={wistiaVideo} allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" autoPlay allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width={windowWidth} height={850}></iframe>
+                <script src="//fast.wistia.net/assets/external/E-v1.js" async></script>
+                <iframe src={wistiaVideo} allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width={windowWidth} height={850}></iframe>
                 {content &&
                     <div className={styles.conatiner}>
                         <div className={styles.heroButtonAndTextContainer}>
@@ -37,11 +38,11 @@ export const WistiaHero = ({ id, content }) => {
         <>
             <script src="https://fast.wistia.com/embed/medias/wjbv2ss2xt.jsonp" async></script>
             <script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>
-            <section class="wistia_responsive_padding" style={{padding:'56.25% 0 0 0',position:'relative'}}>
-                <div class="wistia_responsive_wrapper" style={{height:'100%',left:0,position:'absolute',top:0,width:'100%'}}>
-                    <div class="wistia_embed wistia_async_wjbv2ss2xt videoFoam=true" style={{height:'100%',position:'relative',width:'100%'}}>
-                        <div class="wistia_swatch" style={{height:'100%',left:0,opacity:0,overflow:'hidden',position:'absolute',top:0,transition:'opacity 200ms',width:'100%'}}>
-                            <Image src="https://fast.wistia.com/embed/medias/wjbv2ss2xt/swatch" style={{filter:'blur(5px)',height:'100%',objectFit:'contain',width:'100%'}} alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
+            <section class="wistia_responsive_padding" style={{ padding: '56.25% 0 0 0', position: 'relative' }}>
+                <div class="wistia_responsive_wrapper" style={{ height: '100%', left: 0, position: 'absolute', top: 0, width: '100%' }}>
+                    <div class="wistia_embed wistia_async_wjbv2ss2xt videoFoam=true" style={{ height: '100%', position: 'relative', width: '100%' }}>
+                        <div class="wistia_swatch" style={{ height: '100%', left: 0, opacity: 0, overflow: 'hidden', position: 'absolute', top: 0, transition: 'opacity 200ms', width: '100%' }}>
+                            <Image src="https://fast.wistia.com/embed/medias/wjbv2ss2xt/swatch" style={{ filter: 'blur(5px)', height: '100%', objectFit: 'contain', width: '100%' }} alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" />
                         </div>
                     </div>
                 </div>
