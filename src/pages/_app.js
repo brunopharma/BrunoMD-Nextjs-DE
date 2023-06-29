617ebe262cdd4f3da96f6e8633022331242563f8
NotoriousJat <nishantjat.dx@gmail.com>
2023-06-29 18:06:41 +0530

    init

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import Layout from "@/componets/Layout"
 import { RouteGuard } from '@/componets/RouteGuard'
 export default function App({ Component, pageProps }) {
-  const version = 'EU'
+  const version = 'ENG'
   if (Component?.hideLayout) {
     return (
       <RouteGuard version={version}>
